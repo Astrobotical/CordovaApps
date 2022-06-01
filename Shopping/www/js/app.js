4f93641c3140a2a431b71a0967e3b88ea932fe88
Astrobotical <romari123456789@gmail.com>
2022-05-31 21:43:13 -0500

    Added the view cart functionalities

--- Shopping/www/js/app.js
+++ Shopping/www/js/app.js
@@ -4,6 +4,7 @@ const Foodlist =[{ID:"Pancake", Price: 250, Description: "Pancake with butter an
 {ID: "Pasta" , Price: 350, Description: "Pasta with tomato sauce and meat",    img : 'img/food/pasta.jfif'},
 {ID: "Sandwich" , Price: 400, Description: "Sandwich with ham and cheese",   img : 'img/food/sandwich.jfif'}];
 var container = document.getElementById("app");
+var cartbutton = document.getElementById("cart");
 var storage = window.localStorage;
 var cartitems =[];
 let app ={
@@ -31,14 +32,14 @@ let app ={
         container.appendChild(div);
     }
 },  addtocart : function(id){
-{
-    alert(id + " added to cart");
-    alertify.notify(`${id} added to cart`, 'success', 5, function(){  console.log('dismissed'); });
+    {
+   // alert(id + " added to cart");
+
     Foodlist.forEach((key, value) =>{
-        console.log(key.ID);
+        //console.log(key.ID);
         if(id ==  key.ID){
-           if(cartitems.hasOwnProperty(id)){
-               return  alertify.notify(`${id} is already in the  cart`, 'success', 3, function(){  console.log('dismissed'); });
+           if(cartitems.find(x => x.ID === id)){
+               return  alertify.warning(id +' is already in cart'); 
            }
            else{
                var newly = 
@@ -49,13 +50,55 @@ let app ={
                             img : key.img
                    }
                ;
+               alertify.notify(`${id} added to cart`, 'success', 3, function(){  console.log(`${id} was added`); });
                var cart = document.getElementById("cart");
                 cartitems.push(newly);
-                cart.innerHTML = "Cart Items" + cartitems.length;
+                cart.innerHTML = "Cart Items " + `(${cartitems.length})`;
            }
         }
     });
 }
-}
+},
+    showcart: function(){
+        var cart = document.getElementById("cart");
+        var items = document.getElementById("chartshow");
+        cart.innerHTML = "Cart Items " + `(${cartitems.length})`;
+        var cartlist ="";
+        cartlist += ` 
+        <table class="table">
+        <thead>
+        <tr>
+        <th scope="col">Item Image</th>
+        <th scope="col">Item Name</th>
+        <th scope="col">Item Price</th>
+        <th scope="col">Item Description </th>
+        <th scope="col">Item Action</th> 
+        </thead>
+        <tbody>`;
+        cartitems.forEach((key, value) =>{
+            cartlist += `<tr><th scope="row"><img src="${key.img}" class="card-img" style="height = 20%; width = 20%;" </th>
+            <td>${key.ID}</td>
+            <td>${key.Price}</td>
+            <td>${key.Description}</td>
+            <td><button onclick="app.removefromcart(\``+key.ID+ `\`)" class="btn btn-danger">Remove</button></td></tr>`;
+        });
+        cartlist += `</tbody></table><button onclick="app.hidecart()" class="btn btn-primary">Close</button>`;
+        cartbutton.style.display = "none";
+            items.innerHTML = cartlist;
+    },
+    hidecart: function(){
+        var items = document.getElementById("chartshow");
+        cartbutton.style.display = "block";
+        items.innerHTML = "";
+    },
+    removefromcart: function(id){
+        cartitems.forEach((key, value) =>{
+            if(id ==  key.ID){
+                alertify.notify(`${id} removed from cart`, 'error', 5, function(){  console.log('Item removed'); });
+                cartitems.splice(value, 1);
+            }
+        });
+        app.showcart();
+    }
 };
 app.initialize();
